16e9fb66f74abc98bd9eeac0dd00b692cd0682c2
debabrata4934-hue <debabrata4934@gmail.com>
2026-07-03 20:38:31 +0530

    Add navbar layout and styles

--- frontend/src/commonPages/Navbar.jsx
+++ frontend/src/commonPages/Navbar.jsx
@@ -1,6 +1,21 @@
+import './Navbar.css';
 function Navbar() {
     return(
-        <h1>NavBar</h1>
+        <>
+            <div className="navBar">
+                <div className="logo_location">
+                    <div className="logo">logo</div>
+                    <div className="location">location</div>
+                </div>
+                <div className="searchBar">SearchBar</div>
+                <div className="others_Btn">
+                    <div className="language">language</div>
+                    <div className="signin_AccList">acclist</div>
+                    <div className="return_order">Order</div>
+                    <div className="cart">cart</div>
+                </div>
+            </div>
+        </>
     )
 }
 
